0faf45e0c5ebffc80965d29ac4676d8deca51d8e
RafHon <72348008+RafHon@users.noreply.github.com>
2025-03-04 19:02:26 +0100

    init

---
+++ Model/BasketPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class BasketPosition
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Key]
+        public int UserID { get; set; }
+        public int Amount { get; set; }
+
+        [ForeignKey(nameof(ProductID))]
+        public Product Product { get; set; }
+        [ForeignKey(nameof(UserID))]
+        public User User { get; set; }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public bool IsActive { get; set; }
+
+        [ForeignKey(nameof(GroupID))]
+        public int? GroupID { get; set; }
+
+        public ProductGroup Group { get; set; }
+        public ICollection<BasketPosition> BasketPositions { get; set; }
+        public ICollection<OrderPosition> OrderPositions { get; set; }
+    }
+
+}
